8c40a24407549858cdc24081d7e4952a96de6bb4
kimtaehyung <thpulsek@naver.com>
2017-01-07 12:53:40 +0900

    첫번째 push

---
+++ src/main/java/kitri/main/IndexController.java
@@ -0,0 +1,16 @@
+package kitri.main;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController{
+	@RequestMapping("/index.do")
+	public ModelAndView main(){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		return mav;
+	}
+}
