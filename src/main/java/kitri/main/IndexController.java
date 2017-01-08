2f03a7f393e63f1f9879f40dcc33a57854cab789
soojung <soojung@sj>
2017-01-08 17:18:16 +0900

    VIEW 관리자용 폴더생성(test용jsp생성) content페이지에 박스오피스 정보 뿌리기(test)
    CONFIG
    mybatis-config.xml에서 박스오피스DTO 연결
    
    JAVA
    박스오피스 dto/controller/service/dao/xml(mapper) 생성

---
+++ src/main/java/kitri/main/IndexController.java
@@ -0,0 +1,27 @@
+package kitri.main;
+
+
+import java.util.List;
+
+import kitri.performinfo.boxweekly.dto.BoxWeeklyDTO;
+import kitri.performinfo.boxweekly.service.BoxWeeklyService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController{
+	@Autowired
+	BoxWeeklyService service;
+	
+	@RequestMapping("/index.do")
+	public ModelAndView main(BoxWeeklyDTO dto){
+		// ¹Ú½º¿ÀÇÇ½º ÆÄ½Ì °á°ú »Ñ¸²
+		System.out.println("ÀÎµ¦½º"+dto);
+		List<BoxWeeklyDTO> boxlist = service.Total_BoxWeekly(dto);
+		System.out.println(boxlist);
+		return new ModelAndView("index","boxlist",boxlist);
+	}
+}
